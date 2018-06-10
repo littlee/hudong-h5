c0409fbbd8b61393b74699b944ce296a62049f40
littlee <usedlittlee@gmail.com>
2018-06-10 10:18:22 +0800

    typo

--- src/Pages/Enroll/Result/index.js
+++ src/Pages/Enroll/Result/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Page from 'components/Page';
-class Resutl extends React.Component {
+class Result extends React.Component {
   render() {
     const { images_config } = this.props.data;
     return <Page bg={`url(${images_config.result_bg})`} />;
   }
 }
 
-export default Resutl;
+export default Result;
